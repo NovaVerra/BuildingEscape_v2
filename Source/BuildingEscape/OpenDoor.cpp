b6e156c417080646502b50de94dcdcbfc5d471c4
lununglee <leo.lunung.lee@gmail.com>
2020-02-07 10:49:29 -0800

    added OpenDoor component to open doors relatively

---
+++ Source/BuildingEscape/OpenDoor.cpp
@@ -0,0 +1,38 @@
+// All Rights Reserved Leo Lee 2020
+
+
+#include "OpenDoor.h"
+#include "GameFramework/Actor.h"
+
+// Sets default values for this component's properties
+UOpenDoor::UOpenDoor()
+{
+	// Set this component to be initialized when the game starts, and to be ticked every frame.  You can turn these features
+	// off to improve performance if you don't need them.
+	PrimaryComponentTick.bCanEverTick = true;
+
+	// ...
+}
+
+
+// Called when the game starts
+void UOpenDoor::BeginPlay()
+{
+	Super::BeginPlay();
+
+	float		OpenDoorAngle {90.f};
+
+	FRotator	CurrentRotation {GetOwner()->GetActorRotation()};
+	FRotator	UpdatedRotation {CurrentRotation.Pitch, CurrentRotation.Yaw -= OpenDoorAngle, CurrentRotation.Roll};
+	GetOwner()->SetActorRotation(UpdatedRotation);
+}
+
+
+// Called every frame
+void UOpenDoor::TickComponent(float DeltaTime, ELevelTick TickType, FActorComponentTickFunction* ThisTickFunction)
+{
+	Super::TickComponent(DeltaTime, TickType, ThisTickFunction);
+
+	// ...
+}
+
